d3a45677da92b8c4399ee0d9ad54cb2f1643c5cd
ahmed.elmohame1997.ae@gmail.com <ahmed.elmohame1997.ae@gmail.com>
2020-09-19 19:02:53 +0200

    added share option

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -1,28 +1,13 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
 
 
@@ -33,8 +18,38 @@ class GameWonFragment : Fragment() {
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
 
-        binding.nextMatchButton.setOnClickListener(
-                Navigation.createNavigateOnClickListener(R.id.action_gameWonFragment_to_gameFragment))
+        binding.nextMatchButton.setOnClickListener { view: View ->
+            view.findNavController()
+                    .navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
+        }
+
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.winner_menu, menu)
+    }
+
+    private fun getShareIntent(): Intent {
+        val args = arguments?.let { GameWonFragmentArgs.fromBundle(it) }
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.setType("text/plain")
+                .putExtra(Intent.EXTRA_TEXT,
+                getString(R.string.share_success_text, args!!.numCorrect,args!!.numQuestions))
+
+        return intent
+    }
+
+    private fun share(){
+        startActivity(getShareIntent())
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.share -> share()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
